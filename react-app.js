e81547a0eb3f39f43c48f49f1ed75a8b42a28410
Aman Bhardwaj <amanbhardwaj@Amans-MacBook-Air.local>
2020-05-26 15:00:35 +0530

    react code in seperate file

---
+++ react-app.js
@@ -0,0 +1,23 @@
+
+function App() {
+
+    document.cookie = 'SameSite=None Secure'
+    
+    React.useEffect(() => {
+        stAnalytics.getInstance().sendPageViewEvent('home_page_viewed', {})
+        // eslint-disable-next-line
+    },[])
+    
+    
+    return (
+    <div className="App">
+        <div className="button-container">
+            <a href="#!" className='btn red' onClick={button1Clicked}>button1</a>
+            <a href="#!" className='btn red' onClick={button2Clicked}>button2</a>
+        </div>
+    </div>
+    );
+    }
+    
+    ReactDOM.render(<App />, document.getElementById('root'))
+    
